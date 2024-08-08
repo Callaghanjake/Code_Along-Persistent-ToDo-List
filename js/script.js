3052566ce5eac41843a0f2930108246469ed0edc
Callaghanjake <jake_2024@hotmail.co.uk>
2024-08-08 21:15:21 +0100

    feat: add for loop to gain index, item pairs.

--- js/script.js
+++ js/script.js
@@ -9,9 +9,15 @@ let itemsDiv = document.querySelector('.items');
 // To Do List Functions
 
 function renderItems() {
+    itemsDiv.textContent = null;
 
+    for (let [idx, item] of Object.entries(items)) {
+        let text = document.createElement('p');
+        text.textContent = item;
+
+        itemsDiv.appendChild(text);
+    }
 }
-
 
 function loadItems() {
 
