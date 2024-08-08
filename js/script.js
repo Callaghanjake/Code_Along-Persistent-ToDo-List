f01fc28ffc4a254bb1e7ad5b9319566b3e3d6958
Callaghanjake <jake_2024@hotmail.co.uk>
2024-08-08 21:10:51 +0100

    feat: add basic HTML structure and created skeleton JS.

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Array to stor items
+
+let items = [];
+
+// HTML to JS Variables
+
+let itemsDiv = document.querySelector('.items');
+
+// To Do List Functions
+
+function renderItems() {
+
+}
+
+
+function loadItems() {
+
+}
+
+function saveItems() {
+
+}
+
+function addItems() {
+
+}
+
+function removeItem() {
+
+}
